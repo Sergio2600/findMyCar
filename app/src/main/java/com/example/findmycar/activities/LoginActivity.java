f0a0fe0975fe5781395825c22de3437a91cd2d31
sergio2600 <sergioblazquez10@gmail.com>
2023-04-28 19:03:54 +0200

    terminando actividad login y register

--- app/src/main/java/com/example/findmycar/activities/LoginActivity.java
+++ app/src/main/java/com/example/findmycar/activities/LoginActivity.java
@@ -6,8 +6,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.findmycar.R;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -27,5 +30,18 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
     }
+    public void login(View view) {
+        //creamos dos variables la cual hay que hacer un casting para que devuelva un TextInputEditText
+        EditText usuario = (EditText) findViewById(R.id.cajausuario);
+        EditText pass = (EditText) findViewById(R.id.cajapass);
 
+        if(usuario.getText().toString().length()==0 || pass.getText().toString().length()==0){
+            Toast toast = Toast.makeText(this,"hay algun campo vacio",Toast.LENGTH_LONG);
+            toast.show();
+        }else {//comparamos el contenido de las cajas
+
+        }
+
+
+    }
 }
--- app/src/main/java/com/example/findmycar/activities/Register.java
+++ app/src/main/java/com/example/findmycar/activities/Register.java
@@ -6,16 +6,33 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.findmycar.R;
+import com.example.findmycar.activities.db.ControladorDB;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class Register extends AppCompatActivity {
+    //objeto controladorDB
+    ControladorDB controladorDB = new ControladorDB(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_register);
+
+    }
+
+    //para crear un usuario nuevo
+    public void crearCuentaUsuario(View view){
+        TextInputEditText usuario = (TextInputEditText) findViewById(R.id.cajaUsuarioRegistro);
+        TextInputEditText pass = (TextInputEditText) findViewById(R.id.cajaPassRegistro);
+
+        controladorDB.registrarUsuario(usuario.getText().toString(),pass.getText().toString());
+        Toast toast = Toast.makeText(this,"Usuario registrado correctamente",Toast.LENGTH_LONG);
+        toast.show();
+
 
     }
 }
