c5b2c2bac56b168d672d2dd6f5ba5952305be58f
Daniel Alberich <78300981+danielalberich@users.noreply.github.com>
2023-05-03 13:05:21 +0200

    Empezado a hacer activity de preguntas

--- app/src/main/java/com/example/findmycar/activities/QActivity.java
+++ app/src/main/java/com/example/findmycar/activities/QActivity.java
@@ -3,14 +3,35 @@ package com.example.findmycar.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.findmycar.R;
 
 public class QActivity extends AppCompatActivity {
 
+    private int id_respuestas[] = {
+            R.id.respuesta1, R.id.respuesta2, R.id.respuesta3,
+            R.id.respuesta4
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qactivity);
+
+//Mete los strings en la activity
+//La posición 0 del array siempre será la pregunta
+        String[] stringPregunta = getResources().getStringArray(R.array.pregunta1);
+
+        TextView Quest = (TextView) findViewById(R.id.Quest);
+        Quest.setText(stringPregunta[0]);
+
+//Recorre el array metiendo las stringPregunta
+        for (int i = 0; i < id_respuestas.length; i++){
+            Button respuesta = (Button) findViewById(id_respuestas[i]);
+            respuesta.setText(stringPregunta[i+1]);
+        }
+
     }
 }
