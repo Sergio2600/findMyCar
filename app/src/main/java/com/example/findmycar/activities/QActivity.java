0434d4999c5043933f5d2a8421d31e094ffc2210
sergio2600 <sergioblazquez10@gmail.com>
2023-05-22 19:44:07 +0200

    Resolviendo errores

--- app/src/main/java/com/example/findmycar/activities/QActivity.java
+++ app/src/main/java/com/example/findmycar/activities/QActivity.java
@@ -308,19 +308,16 @@ public class QActivity extends AppCompatActivity {
                 btn5.setVisibility(View.GONE);
                 btn6.setVisibility(View.GONE);
                 break;
-<<<<<<< HEAD
             case 5: //4 respuestas
                 //btn5.setVisibility(View.GONE);
                 //btn6.setVisibility(View.GONE);
-=======
             case 4: // 4 respuestas
                 btn5.setVisibility(View.GONE);
                 btn6.setVisibility(View.GONE);
->>>>>>> master
                 break;
-            case 5: // 5 respuestas
-                btn6.setVisibility(View.GONE);
-                break;
+           // case 5: // 5 respuestas
+              //  btn6.setVisibility(View.GONE);
+               // break;
             case 6: // 6 respuestas
                 // No es necesario ocultar ningún botón
                 break;
