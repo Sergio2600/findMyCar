0622570c654534ac46460633f937cc66a13c84ab
Daniel Alberich <78300981+danielalberich@users.noreply.github.com>
2023-05-23 11:40:59 +0200

    ResultadosActivity hecha (pero mal)

--- app/src/main/java/com/example/findmycar/activities/ResultadosActivity.java
+++ app/src/main/java/com/example/findmycar/activities/ResultadosActivity.java
@@ -3,13 +3,20 @@ package com.example.findmycar.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
 import com.example.findmycar.R;
+import com.example.findmycar.activities.db.MyDatabaseHelper;
 
 public class ResultadosActivity extends AppCompatActivity {
 
     private String valorPrecio, tipoCarroceria, tipoCombustible, valorPotencia, tipoDistintivo, valorMaletero;
+
+    //Esto da problemas, no está bien hehco
+    //MyDatabaseHelper dbHelper = new MyDatabaseHelper(this);
+    //SQLiteDatabase db = dbHelper.getReadableDatabase();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,5 +30,50 @@ public class ResultadosActivity extends AppCompatActivity {
         valorPotencia = intent.getStringExtra("potencia");
         tipoDistintivo = intent.getStringExtra("distintivo");
         valorMaletero = intent.getStringExtra("maletero");
+
+//TODO Por ahora no funciona, revisar conexión con bbdd conociendola
+        /*
+//Consulta para comparar los datos que tenía con los de las base de datos para ver qué coche coger
+        String tableName = "coches";
+        String[] projection = {"nombre", "imagen"};
+        String selection = "precio = ? AND carroceria = ? AND combustible = ? AND potencia = ? AND distintivo = ? AND maletero = ?";
+        String[] selectionArgs = {valorPrecio, tipoCarroceria, tipoCombustible, valorPotencia, tipoDistintivo, valorMaletero};
+
+        Cursor cursor = db.query(tableName, projection, selection, selectionArgs, null, null, null);
+
+
+//Meter datos de la consulta en una lista
+
+        List<String> nombresCoches = new ArrayList<>();
+        List<Integer> imagenesCoches = new ArrayList<>();
+
+        while (cursor.moveToNext()) {
+            String nombre = cursor.getString(cursor.getColumnIndexOrThrow("nombre"));
+            int imagen = cursor.getInt(cursor.getColumnIndexOrThrow("imagen"));
+
+            nombresCoches.add(nombre);
+            imagenesCoches.add(imagen);
+        }
+
+//Utilizar los datos obtenidos para mostrarlos en el layout
+
+        TextView textView1 = findViewById(R.id.textView1);
+        ImageView imageView1 = findViewById(R.id.imageView1);
+        textView1.setText(nombresCoches.get(0));
+        imageView1.setImageResource(imagenesCoches.get(0));
+
+        TextView textView2 = findViewById(R.id.textView2);
+        ImageView imageView2 = findViewById(R.id.imageView2);
+        textView2.setText(nombresCoches.get(1));
+        imageView2.setImageResource(imagenesCoches.get(1));
+
+        TextView textView3 = findViewById(R.id.textView3);
+        ImageView imageView3 = findViewById(R.id.imageView3);
+        textView3.setText(nombresCoches.get(2));
+        imageView3.setImageResource(imagenesCoches.get(2));
+
+
+*/
+
     }
 }
