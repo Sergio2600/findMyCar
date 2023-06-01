b070c81fff64453ef084b8d5add8eda8037e5207
sergio2600 <sergioblazquez10@gmail.com>
2023-06-01 16:16:24 +0200

    poniendo comentarios en las clases

--- app/src/main/java/com/example/findmycar/activities/ResultadosActivity.java
+++ app/src/main/java/com/example/findmycar/activities/ResultadosActivity.java
@@ -40,64 +40,24 @@ public class ResultadosActivity extends AppCompatActivity {
         tipoDistintivo = intent.getStringExtra("distintivo");
         valorMaletero = intent.getStringExtra("maletero");
 
+        //ponemos las respuestas en un array
         String[] respuestas = {valorPrecio,tipoCarroceria,tipoCombustible,valorPotencia,tipoDistintivo,valorMaletero};
+        // comprobamos que el array de respuestas no esté vacio y que la consulta devuelva algun resultado
         if (respuestas != null && respuestas.length > 0 && controladorDB.buscar(respuestas) != null) {
+                /* cargamos los datos obtenidos en la pantalla en un listview
+                 usando la clase auxiliar ResultadoAdapter */
                 ListView listView = findViewById(R.id.listViewOpciones);
                 ResultadoAdapter adapter = new ResultadoAdapter(this, controladorDB.buscar(respuestas));
                 listView.setAdapter(adapter);
+             // mostramos un texto si se han encontrado coches
             TextView textViewError = findViewById(R.id.textViewError);
             textViewError.setVisibility(View.VISIBLE);
-            textViewError.setText("Estos son los vehiculos que hemos encontrado con tus filtros");
+            textViewError.setText("Estos son los vehiculos que hemos encontrado con tus filtros:");
         } else {
+            // si no hay resultados ponemos otro texto
             TextView textViewError = findViewById(R.id.textViewError);
             textViewError.setVisibility(View.VISIBLE);
             textViewError.setText("No se han encontrado coches con esos filtros, intente con otros filtros");
         }
-
-
-//TODO Por ahora no funciona, revisar conexión con bbdd conociendola
-        /*
-//Consulta para comparar los datos que tenía con los de las base de datos para ver qué coche coger
-        String tableName = "coches";
-        String[] projection = {"nombre", "imagen"};
-        String selection = "precio = ? AND carroceria = ? AND combustible = ? AND potencia = ? AND distintivo = ? AND maletero = ?";
-        String[] selectionArgs = {valorPrecio, tipoCarroceria, tipoCombustible, valorPotencia, tipoDistintivo, valorMaletero};
-
-        Cursor cursor = db.query(tableName, projection, selection, selectionArgs, null, null, null);
-
-
-//Meter datos de la consulta en una lista
-
-        List<String> nombresCoches = new ArrayList<>();
-        List<Integer> imagenesCoches = new ArrayList<>();
-
-        while (cursor.moveToNext()) {
-            String nombre = cursor.getString(cursor.getColumnIndexOrThrow("nombre"));
-            int imagen = cursor.getInt(cursor.getColumnIndexOrThrow("imagen"));
-
-            nombresCoches.add(nombre);
-            imagenesCoches.add(imagen);
-        }
-
-//Utilizar los datos obtenidos para mostrarlos en el layout
-
-        TextView textView1 = findViewById(R.id.textView1);
-        ImageView imageView1 = findViewById(R.id.imageView1);
-        textView1.setText(nombresCoches.get(0));
-        imageView1.setImageResource(imagenesCoches.get(0));
-
-        TextView textView2 = findViewById(R.id.textView2);
-        ImageView imageView2 = findViewById(R.id.imageView2);
-        textView2.setText(nombresCoches.get(1));
-        imageView2.setImageResource(imagenesCoches.get(1));
-
-        TextView textView3 = findViewById(R.id.textView3);
-        ImageView imageView3 = findViewById(R.id.imageView3);
-        textView3.setText(nombresCoches.get(2));
-        imageView3.setImageResource(imagenesCoches.get(2));
-
-
-*/
-
     }
 }
